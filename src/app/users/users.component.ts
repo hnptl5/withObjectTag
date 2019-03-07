07c647b78f302661b3516f15ed7be8717e877c4c
Hani Patel <hani.269274@ad.infosys.com>
2019-03-07 12:34:15 -0500

    changes

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-// import { DynamicComponentLoader } from './../dynamic-component-loader/dynamic-component-loader.service';
-// import { Router, ActivatedRoute, Route } from '@angular/router';
-// import { Injectable, Compiler } from '@angular/core';
-// import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-users',
@@ -15,12 +11,4 @@ export class UsersComponent {
   constructor(){
         console.log('component has been loaded');
   }
-
-  // ngOnInit() {
-  //   // this.loadComponent(this.route.url);
-  // }
-
-  // ngAfterViewInit (){
-  //   this.loadComponent('users');
-  // }
 }
